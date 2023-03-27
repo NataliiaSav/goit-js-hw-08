383f8ec2b0ea8e0c9b3a504a0c43b87e11176277
NataliiaSav <savchenko6561@gmail.com>
2023-03-27 18:08:57 +0300

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-
-    player.setCurrentTime(30.456).then(function(seconds) {
-        // seconds = the actual time that the player seeked to
-    }).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
+function onPlay({seconds}) {
+        localStorage.setItem(STORAGE_KEY, seconds)
+    };
     
-            default:
-                // some other error occurred
-                break;
-        }
-    });
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
+    
