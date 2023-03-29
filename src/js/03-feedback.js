9c4d768ce8d50d2ec08758bfa082694c4580ff8a
NataliiaSav <savchenko6561@gmail.com>
2023-03-29 08:33:46 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onDataInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+function onDataInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function initForm() {
+  let savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+    Object.entries(savedData).forEach(([name, value]) => {
+      console.log(name, value);
+      formData[name] = value;
+      formEl.elements[name].value = value;
+    });
+  }
+}
