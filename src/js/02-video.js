a7df56554395db6f5826df9b3ffc255000030073
NataliiaSav <savchenko6561@gmail.com>
2023-04-02 13:04:08 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,3 +12,20 @@ function onPlay({ seconds }) {
 }
 
 player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+
+// player
+//   .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
